56678ce03ad95ac98a30e4f67a6e364272b7bd95
RabiBirbal <rabibirbal450@gmail.com>
2024-05-03 12:47:04 +0545

    Updated

--- src/pages/academics/[academic].tsx
+++ src/pages/academics/[academic].tsx
@@ -13,7 +13,7 @@ const AcademicPrograms = () => {
       <AppLayout>
         <Metatag
           heading={`School`}
-          subheading="Academic"
+          subheading="Academic Advising"
           og_image={`https://media.istockphoto.com/id/577971232/photo/young-nepali-boys-in-classroom-bhaktapur.jpg?s=612x612&w=0&k=20&c=o4shD8gBdj02sFjLJLZXFJKMU5xYMJ26FPb11CVaKKE=`}
           description={` Explore our diverse undergraduate and graduate programs, state-of-the-art facilities, and vibrant campus life. Join us in shaping the leaders of tomorrow.`}
         />
@@ -22,7 +22,7 @@ const AcademicPrograms = () => {
           data={{
             image:
               "https://www.andreeharpur.com/wp-content/uploads/2018/08/csp-base-page.jpg",
-            title: "Academics",
+            title: "Academics Advising",
           }}
         />
         <div className="layout component-padding">
